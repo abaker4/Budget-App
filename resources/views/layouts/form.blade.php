252b1e215536c9a82b0b3dcebac1e044f2a3d5b8
Austin Baker <baker.austin.c@gmail.com>
2017-08-02 12:28:22 -0400

    Added Daily Categories to view and changed the order

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -29,14 +29,13 @@
                         <p class="control clearl">
                              <span class="select">
                                <select name="daily_category_id">
-                                   <option value="1">Groceries</option>
-                                   <option value="2">Restaurants</option>
-                                   <option value="3">Alcohol/Bars</option>
-                                   <option value="4">Coffee Shops</option>
-                                   <option value="5">Gas/Fuel</option>
-                                   <option value="6">Clothing</option>
-                                   <option value="7">Fast Food</option>
-
+                                   <option value="1">Restaurants</option>
+                                   <option value="2">Alcohol/Bars</option>
+                                   <option value="3">Coffee Shops</option>
+                                   <option value="4">Clothing</option>
+                                   <option value="5">Fast Food</option>
+                                   <option value="6">Groceries</option>
+                                   <option value="7">Gas/Fuel</option>
                                </select>
                              </span>
                         </div>
